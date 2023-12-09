d3587af4e7509c748ce32a3062f609d5d61e3f04
Altobert <alberto.sanmartin@usach.cl>
2023-12-09 14:19:54 -0300

    entitie

--- loa-backend/backend.loa/src/main/java/cl/usach/backend/loa/controller/ControllerLoaAsignaturas.java
+++ loa-backend/backend.loa/src/main/java/cl/usach/backend/loa/controller/ControllerLoaAsignaturas.java
@@ -1,5 +1,8 @@
 package cl.usach.backend.loa.controller;
 
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class ControllerLoaAsignaturas {
 
 }
---
+++ loa-backend/backend.loa/src/main/java/cl/usach/backend/loa/entity/PlanStudy.java
@@ -0,0 +1,10 @@
+package cl.usach.backend.loa.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+
+@Entity
+@Data
+public class PlanStudy {
+
+}
